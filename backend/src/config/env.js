3e518d872927c8fce998d24b7e17c568710be8b0
Amiya Ranjan Maharana <amiyaranjanmaharana2005@gmail.com>
2025-11-01 14:39:58 +0530

    Cloudinary task completed

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,4 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET || 'change-me-in-prod';
 export const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/cleanit_dev';
+export const CLOUD_NAME = process.env.CLOUD_NAME;
+export const API_KEY = process.env.API_KEY;
+export const API_SECRET = process.env.API_SECRET;
---
+++ backend/src/controllers/cloudinaryController.js
@@ -0,0 +1,49 @@
+import { deleter, uploader } from "../services/cloudinaryService.js";
+
+export const uploadHandler  = async (req, res) => {
+  let img = req.files?.img;
+  const {folder} = req.body;
+  
+  try {
+    if (img) {
+        console.log("Image received");
+        console.log("Folder : ", folder);
+        const up = await uploader(img, folder);
+        console.log("Img upload successful:", up);
+    }
+
+    return res.status(200).json({
+        message : "Img upload successful",
+        success : true
+    })
+
+  } catch (error) {
+    console.error("Img upload failed:", error);
+    return res.status(500).json({
+        message : "Img upload failed",
+        success : false
+    })
+  }
+};
+
+export const deleteHandler = async(req, res) => {
+  const {public_id} = req.body;
+  try {
+    if (public_id) {
+        await deleter(public_id);
+        console.log("Img deletion successful");
+    }
+
+    return res.status(200).json({
+        message : "Img deletion successful",
+        success : true
+    })
+
+  } catch (error) {
+    console.error("Img deletion failed:", error);
+    return res.status(500).json({
+        message : "Img deletion failed",
+        success : false
+    })
+  }
+}
