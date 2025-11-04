f0a96494ee8c77337e1a18d14c50c159421192dc
Amrutanshu <amrutanshumishra607@gmail.com>
2025-11-04 23:55:51 +0530

    adding the protect middleware

--- backend/src/routes/robloxflowRoutes.js
+++ backend/src/routes/robloxflowRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { analyzeImage } from "../controllers/robloxflowController.js";
+import { authenticateToken, authorizeRoles } from "../middleware/auth.js";
 
 const router = express.Router();
-router.post("/analyze", analyzeImage);
+router.post("/analyze", authenticateToken, authorizeRoles("employee"),analyzeImage);
 
 export default router;
